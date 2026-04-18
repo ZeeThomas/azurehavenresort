22a4a954200339e3fb21bb3c2adad7e7eda8a414
Zee Thomas <missz91102@gmail.com>
2026-04-18 17:17:48 -0400

    Updated home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -10,7 +10,7 @@ export const Home = () =>
             <Navbar />
             <Heroimage />
             {/*<WeatherStrip /> */}
-            <Bookingbar>
+            <Bookingbar/>
         </div>
     );
 }
