8128923ad148e4de2d357ffb41d9087df6ad6f3f
Zee Thomas <missz91102@gmail.com>
2026-04-18 17:16:43 -0400

    Updated bookingbar.tsx

---
+++ src/pages/home/bookingbar.tsx
@@ -0,0 +1,12 @@
+import '../../styles/heroimage.css';
+import React from "react";
+
+export const Bookingbar = () =>
+{
+    return(
+        <div className="hero-container">
+          <p>Hello World</p>
+        </div>
+        
+    );
+}
